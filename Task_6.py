f1381beac41eed411cb97f7ced55ee2bedd5d500
Evgeniy <e.mastyukov@mail.ru>
2022-08-16 20:01:34 +0300

    Add change to Task_6

--- Task_6.py
+++ Task_6.py
@@ -2,6 +2,4 @@
 num = int(input("Введите число: "))
 if ((num % 5 == 0 and num % 10 == 0) or num % 15 == 0) and not num % 30 == 0:
     print(f"Число кратное 5 и 10 или 15, но не 30 \n = {num}")
-# a = num/15
-# print(a)
-# print(type(a))
+
