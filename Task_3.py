6ad16dcbb9a4b4a0c2c9496818ff0cf9c64c108c
Evgeniy <e.mastyukov@mail.ru>
2022-08-16 19:48:07 +0300

    Add change to Task_3

--- Task_3.py
+++ Task_3.py
@@ -1,5 +1,5 @@
-# 2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.    
-#     Примеры:    
+# 2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
+#     Примеры:
 #     - 1, 4, 8, 7, 5 -> 8
 #     - 78, 55, 36, 90, 2 -> 90
 # n = 0
@@ -7,10 +7,10 @@
 # length = 5
 # index = 0
 # while(n != length):
-#     numbers[index] = int(input(f'Введите {index + 1} число + Enter  '))                                     
+#     numbers[index] = int(input(f'Введите {index + 1} число + Enter  '))
 #     index += 1
 #     n +=1
-# print(numbers)    
+# print(numbers)
 
 num1 = int(input('Введите первое число: '))
 num2 = int(input('Введите второе число: '))
@@ -25,7 +25,6 @@ if max < num3:
 if max < num4:
     max = num4
 if max < num5:
-    max = num5  
-    
-print(f"max = {max}")             
+    max = num5
 
+print(f"max = {max}")
